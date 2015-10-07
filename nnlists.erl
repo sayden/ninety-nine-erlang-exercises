2ccd82e3df2c66546af1fa65f8ea2985092abf66
Mario Castro <mariocaster@gmail.com>
2015-10-07 23:14:02 +0200

    Problem 6 - Check if a list is a palindrome

--- nnlists.erl
+++ nnlists.erl
@@ -10,18 +10,18 @@
 -author("mariocaster").
 
 %% API
--export([myLast/1, myButLast/1, elementAt/2, myLength/1, myReverse/1]).
+-export([last/1, butLast/1, elementAt/2, length/1, reverse/1, isPalindrome/1]).
 
 %% 1 - Find the last element of a list
-%% myLast([1,2,3,4,5]) -> 5
-myLast([]) -> [];
-myLast([L]) -> L;
-myLast([_|List]) -> myLast(List).
+%% Last([1,2,3,4,5]) -> 5
+last([]) -> [];
+last([L]) -> L;
+last([_|List]) -> last(List).
 
 %% 2 - Find the last but one element of a list.
-%% myButLast([1,2,3,4,5]) -> 4
-myButLast([A,_]) -> A;
-myButLast([_|List]) -> myButLast(List).
+%% ButLast([1,2,3,4,5]) -> 4
+butLast([A,_]) -> A;
+butLast([_|List]) -> butLast(List).
 
 %% 3 -  Find the K'th element of a list. The first element in the list is number 1:
 %% elementAt(2,[5,4,3,2,1]) -> 4
@@ -29,12 +29,16 @@ elementAt(1, [L|_]) -> L;
 elementAt(N, [_|List]) -> elementAt(N-1,List).
 
 %% 4 - Find the number of elements of a list.
-myLength([]) -> 0;
-myLength([_|List]) -> 1 + myLength(List).
+length([]) -> 0;
+length([_|List]) -> 1 + nnlists:length(List).
 
 %% 5 - Reverse a list
-myReverse([]) -> [];
-myReverse([H|List]) -> lists:concat([myReverse(List),H]).
+reverse([]) -> [];
+reverse([H|List]) -> lists:append(reverse(List),[H]).
 
 %% 6 - Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+isPalindrome(List) -> List == reverse(List).
 
+%% 7 - Flatten a nested list structure.
+flatten([_]) -> [];
+flatten()
